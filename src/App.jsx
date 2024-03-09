276c55cedcbbf2e2f3a419319e1d87a6bf8997a7
Kashish Durgiya <kdurgiya@gmail.com>
2024-03-09 12:23:02 +0530

    <WIP> adding routing in react app for employee mgmt frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+            {/* http://localhost:3000 */}
+            <Route path='/' element = {<ListEmployeeComponent/>}></Route>
+            {/* http://localhost:3000/employees */}
+            <Route path='/employees' element = {<ListEmployeeComponent/>}></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
